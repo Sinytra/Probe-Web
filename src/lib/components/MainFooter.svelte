33566b6f0b43edd7ebc2dfc5dcec4408d0c27fd9
Su5eD <su5ed@outlook.com>
2025-09-06 18:00:10 +0200

    Add LICENSE

--- src/lib/components/MainFooter.svelte
+++ src/lib/components/MainFooter.svelte
@@ -5,7 +5,10 @@
 
 <div class="container">
 	<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-		<p class="col-md-4 mb-0 text-body-secondary">© 2025 Sinytra</p>
+		<div class="text-nowrap me-auto">
+			<p class="col-md-4 mb-0 text-body-secondary">© 2025 Sinytra</p>
+			<span class="small">This website is <a href="https://github.com/Sinytra/Probe-Web">open source</a>.</span>
+		</div>
 
 		<a
 			href="/"
@@ -23,9 +26,9 @@
 				<NavLink href="/compatibility" class="px-2 text-body-secondary">Compatibility</NavLink>
 			</NavItem>
 			<NavItem>
-				<NavLink href="/compatibility/archive" class="px-2 text-body-secondary"
-					>All releases</NavLink
-				>
+				<NavLink href="/compatibility/archive" class="px-2 text-body-secondary">
+					All releases
+				</NavLink>
 			</NavItem>
 			<NavItem>
 				<NavLink href="/about" class="px-2 text-body-secondary">About</NavLink>
