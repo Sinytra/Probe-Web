6d3e6e41f529df77734c32d0dd4876ea7d682359
Su5eD <su5ed@outlook.com>
2025-09-07 14:17:15 +0200

    Add Twitter metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,15 +7,33 @@
 	import MainFooter from '$lib/components/MainFooter.svelte';
 
 	let { children } = $props();
+
+	let title = 'Sinytra Connector';
+	let description = 'Run your favourite Fabric mods on NeoForge';
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 	<script src={scriptSrc}></script>
 
-	<meta name="og:title" content="Sinytra Connector" />
-	<meta name="og:description" content="Run your favourite Fabric mods on NeoForge" />
+	<meta name="description" content={description} />
+	<meta name="og:title" content={title} />
+	<meta name="og:description" content={description} />
 	<meta name="og:image" content="/banner.png" />
+	<meta name="og:image:alt" content={title} />
+	<meta name="og:image:type" content="image/png" />
+	<meta name="og:image:width" content="1280" />
+	<meta name="og:image:height" content="640" />
+	<meta name="og:type" content="website" />
+
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+	<meta name="twitter:image" content="/banner.png" />
+	<meta name="twitter:image:alt" content={title} />
+	<meta name="twitter:image:type" content="image/png" />
+	<meta name="twitter:image:width" content="1280" />
+	<meta name="twitter:image:height" content="640" />
 </svelte:head>
 
 <MainNavbar />
