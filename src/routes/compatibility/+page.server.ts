0c94e469ac64fb30e8fab55663feb6c80ce052c7
Su5eD <su5ed@outlook.com>
2025-09-30 20:35:26 +0200

    Fix hardcoded tag on latest test results

--- src/routes/compatibility/+page.server.ts
+++ src/routes/compatibility/+page.server.ts
@@ -1,9 +1,15 @@
 import type { PageServerLoad } from './$types';
 import type { TestReport } from '$lib/types';
+import { getLatestTestRelease } from '$lib/github';
+import { error } from '@sveltejs/kit';
+import { GITHUB_LINK_CONNECTOR } from '$lib';
 
 export const load: PageServerLoad = async () => {
-	const tagName = '2.0.0-beta.9+1.21.1';
-	const testsURL = `https://github.com/Sinytra/Connector/releases/download/${tagName}/results.json`;
+	const latest = await getLatestTestRelease();
+	if (!latest) return error(404);
+
+	const { tagName } = latest;
+	const testsURL = `${GITHUB_LINK_CONNECTOR}/releases/download/${tagName}/results.json`;
 	const resp = await fetch(testsURL);
 	const report = (await resp.json()) as TestReport;
 
